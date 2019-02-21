4f6f26697561cc0e3d664583ca72bd717026069e
SerganO <sergeu.ostrovetskiu.2001@gmail.com>
2019-02-21 12:49:23 +0200

    searchBarStyle, appIcons

--- StoreSearch/StoreSearch/AppDelegate.swift
+++ StoreSearch/StoreSearch/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        customizeAppearance()
         return true
     }
 
@@ -87,6 +88,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
             }
         }
+    }
+    
+    
+    //__________ Appearance __________
+    
+    func customizeAppearance()
+    {
+        let barTintColor = UIColor(red: 20/255, green:160/255, blue: 160/255, alpha: 1)
+        UISearchBar.appearance().barTintColor = barTintColor
+        window!.tintColor = UIColor(red: 10/255, green: 80/255, blue: 80/255, alpha: 1)
     }
 
 }
