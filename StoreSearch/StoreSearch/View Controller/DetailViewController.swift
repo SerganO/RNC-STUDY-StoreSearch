8bbf2187998c60e1f8008fbd67b71b2afb3aa7c8
SerganO <sergeu.ostrovetskiu.2001@gmail.com>
2019-02-22 13:37:28 +0200

    Pop-up(start)

---
+++ StoreSearch/StoreSearch/View Controller/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  StoreSearch
+//
+//  Created by Trainee on 2/22/19.
+//  Copyright © 2019 Trainee. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController
+{
+
+    
+    //__________ Load __________
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+    //__________ Actions __________
+    
+    @IBAction func close()
+    {
+        dismiss(animated: true, completion: nil)
+    }
+
+    
+}
