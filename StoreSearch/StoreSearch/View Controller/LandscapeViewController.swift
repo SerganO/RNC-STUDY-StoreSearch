495f4e3f02682ac1b60abdd53414922f7a8690d4
SerganO <sergeu.ostrovetskiu.2001@gmail.com>
2019-02-25 16:49:26 +0200

    Landscape, scroll

--- StoreSearch/StoreSearch/View Controller/LandscapeViewController.swift
+++ StoreSearch/StoreSearch/View Controller/LandscapeViewController.swift
@@ -10,48 +10,68 @@ import UIKit
 
 class LandscapeViewController: UIViewController
 {
-
-    //__________ Variable __________
-    
-    var searchResults = [SearchResult]()
-    private var firstTime = true
     
     //__________ Outlet __________
-    
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var pageControl: UIPageControl!
     
-    //__________ Outlet __________
+    //__________ Variable __________
+    private var firstTime = true
+    private var downloads = [URLSessionDownloadTask]()
+    var searchResults = [SearchResult]()
     
-    @IBAction func pageChanged(_ sender: UIPageControl)
-    {
-        UIView.animate(withDuration: 0.3, delay: 0,
-            options: [.curveEaseInOut], animations:
-            {self.scrollView.contentOffset = CGPoint(x: self.scrollView.bounds.size.width * CGFloat(sender.currentPage), y: 0)},completion: nil)
-        
-    }
     
     //__________ Load __________
-    
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "LandscapeBackground")!)
+
         view.removeConstraints(view.constraints)
         view.translatesAutoresizingMaskIntoConstraints = true
-       
+
         pageControl.removeConstraints(pageControl.constraints)
         pageControl.translatesAutoresizingMaskIntoConstraints = true
-        
+        pageControl.numberOfPages = 0
+
         scrollView.removeConstraints(scrollView.constraints)
         scrollView.translatesAutoresizingMaskIntoConstraints = true
-        
-        view.backgroundColor = UIColor(patternImage: UIImage(named: "LandscapeBackground")!)
-        pageControl.numberOfPages = 0
     }
     
+    override func viewWillLayoutSubviews()
+    {
+        super.viewWillLayoutSubviews()
+        let safeFrame = view.safeAreaLayoutGuide.layoutFrame
+        scrollView.frame = safeFrame
+        pageControl.frame = CGRect(x: safeFrame.origin.x,
+                                   y: safeFrame.size.height - pageControl.frame.size.height,
+                                   width: safeFrame.size.width,
+                                   height: pageControl.frame.size.height)
+        if firstTime
+        {
+            firstTime = false
+            tileButtons(searchResults)
+        }
+    }
     
-    private func titleButtons(_ searchResults: [SearchResult])
+    deinit
+    {
+        print("deinit \(self)")
+        for task in downloads
+        {
+            task.cancel()
+        }
+    }
+    
+    //__________ Actions __________
+    @IBAction func pageChanged(_ sender: UIPageControl)
+    {
+        UIView.animate(withDuration: 0.3, delay: 0, options: [.curveEaseInOut], animations: {
+            self.scrollView.contentOffset = CGPoint(x: self.scrollView.bounds.size.width * CGFloat(sender.currentPage), y: 0)
+        }, completion: nil)
+    }
+    
+    private func tileButtons(_ searchResults: [SearchResult])
     {
         var columnsPerPage = 6
         var rowsPerPage = 3
@@ -59,10 +79,10 @@ class LandscapeViewController: UIViewController
         var itemHeight: CGFloat = 88
         var marginX: CGFloat = 2
         var marginY: CGFloat = 20
+        
         let viewWidth = scrollView.bounds.size.width
-
-        switch viewWidth
-        {
+        
+        switch viewWidth {
         case 568:
             // 4-inch device
             break
@@ -94,27 +114,22 @@ class LandscapeViewController: UIViewController
         default:
             break
         }
-        
-        
+
         let buttonWidth: CGFloat = 82
         let buttonHeight: CGFloat = 82
         let paddingHorz = (itemWidth - buttonWidth)/2
         let paddingVert = (itemHeight - buttonHeight)/2
-        
+
         var row = 0
         var column = 0
         var x = marginX
-        for (index, result) in searchResults.enumerated()
+        for (_, result) in searchResults.enumerated()
         {
-
-            let button = UIButton(type: .system)
-            button.backgroundColor = UIColor.white
-            button.setTitle("\(index)", for: .normal)
-
+            let button = UIButton(type: .custom)
+            button.setBackgroundImage(UIImage(named: "LandscapeButton"), for: .normal)
+            downloadImage(for: result, andPlaceOn: button)
             button.frame = CGRect(x: x + paddingHorz, y: marginY + CGFloat(row)*itemHeight + paddingVert, width: buttonWidth, height: buttonHeight)
-
             scrollView.addSubview(button)
-            
             row += 1
             if row == rowsPerPage
             {
@@ -125,32 +140,42 @@ class LandscapeViewController: UIViewController
                     column = 0; x += marginX * 2
                 }
             }
-        
         }
+
         let buttonsPerPage = columnsPerPage * rowsPerPage
         let numPages = 1 + (searchResults.count - 1) / buttonsPerPage
-        scrollView.contentSize = CGSize(width: CGFloat(numPages) * viewWidth,height: scrollView.bounds.size.height)
+        scrollView.contentSize = CGSize(
+            width: CGFloat(numPages) * viewWidth,
+            height: scrollView.bounds.size.height)
         
         print("Number of pages: \(numPages)")
         pageControl.numberOfPages = numPages
         pageControl.currentPage = 0
     }
     
-    override func viewWillLayoutSubviews()
+    private func downloadImage(for searchResult: SearchResult, andPlaceOn button: UIButton)
     {
-        super.viewWillLayoutSubviews()
-        let safeFrame = view.safeAreaLayoutGuide.layoutFrame
-        scrollView.frame = safeFrame
-        pageControl.frame = CGRect(x: safeFrame.origin.x,y: safeFrame.size.height - pageControl.frame.size.height,width: safeFrame.size.width,height: pageControl.frame.size.height)
-        if firstTime
+        if let url = URL(string: searchResult.imageSmall)
         {
-            firstTime = false
-            titleButtons(searchResults)
+            let task = URLSession.shared.downloadTask(with: url) {[weak button] url, response, error in
+                if error == nil, let url = url,
+                    let data = try? Data(contentsOf: url),
+                    let image = UIImage(data: data)
+                {
+                    DispatchQueue.main.async
+                        {
+                        if let button = button
+                        {
+                            button.setImage(image, for: .normal)
+                        }
+                    }
+                }
+            }
+            task.resume()
+            downloads.append(task)
         }
     }
-    
 }
-
 
 //__________ Extension __________
 
